ce328af64de7e7f5bc0c05e4354d515cc719c449
Breens-Thomas <mbakabreens@gmail.com>
2020-12-13 17:01:24 +0300

    A control flow statement that has a main condition that must be true so that the nested if/else statements can be executed.This sees if the day someone was born give the Akan name by accesing the names using brackets and indexes

--- js/script.js
+++ js/script.js
@@ -13,4 +13,29 @@ function akanNaming() {
     var century = Math.trunc(Math.round(year * 0.01));
     //calculation to know which day of the week the user was born
     var dayOfTheWeek = Math.round((((century/4)-2*century-1)+((5*year/4))+((26*(month+1)/10))+day)%7);
+
+    //this condition must be true so that the nested if/else if statements can be executed
+    if(gender == "male") {
+        if(dayOfTheWeek === 0) {
+            alert("Hey your Akan name is: " + akanMaleNames[0]);
+        }
+        else if(dayOfTheWeek === 1) {
+            alert("Hey your Akan name is: " + akanMaleNames[1]);
+        }
+        else if(dayOfTheWeek === 2) {
+            alert("Hey your Akan name is: " + akanMaleNames[2]);
+        }
+        else if(dayOfTheWeek === 3) {
+            alert("Hey your Akan name is: " + akanMaleNames[3]);
+        }
+        else if(dayOfTheWeek === 4) {
+            alert("Hey your Akan name is: " + akanMaleNames[4]);
+        }
+        else if(dayOfTheWeek === 5) {
+            alert("Hey your Akan name is: " + akanMaleNames[5]);
+        }
+        else {
+            alert("Hey your Akan name is: " + akanMaleNames[6]);
+        }
+    }
 }
