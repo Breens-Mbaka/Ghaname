54bda3b8b397f4cbbcfe40b53f082c958cb53c0f
Breens-Thomas <mbakabreens@gmail.com>
2020-12-13 11:10:00 +0300

    Added a form and also added a function that will be used to allocate Akan names

--- js/script.js
+++ js/script.js
@@ -1,33 +1,3 @@
-//function for male naming
-function maleNaming(century,year,month,day) {
-    var names = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
-    var dayOfTheWeek = Math.trunc(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day )) % 7;
-    if (dayOfTheWeek === 0) {
-        alert("Hello " + names[0]);
-    }
-    else if(dayOfTheWeek === 1) {
-        alert("Hello " + names[1]);
-    }
-    else if(dayOfTheWeek === 2) {
-        alert("Hello " + names[2]);
-    }
-    else if(dayOfTheWeek === 3) {
-        alert("Hello " + names[3]);
-    }
-    else if(dayOfTheWeek === 4) {
-        alert("Hello " + names[4]);
-    }
-    else if(dayOfTheWeek === 5) {
-        alert("Hello " + names[5]);
-    }
-    else {
-        alert("Hello " + names[6]);
-    }
+function akanNaming() {
+    
 }
-
-var century = parseInt(prompt("Century"));
-var year = parseInt(prompt("Year"));
-var month = parseInt(prompt("Month"));
-var day = parseInt(prompt("day"));
-
-maleNaming(century,year,month,day);
