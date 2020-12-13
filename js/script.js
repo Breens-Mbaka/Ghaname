ebde0ec4a3117af74a7585abeed90f8920cbbd1c
Breens-Thomas <mbakabreens@gmail.com>
2020-12-13 16:42:17 +0300

    Get the information entered by user by the help of a '.value' property

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ function akanNaming() {
     var akanMaleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     var akanFemaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
-    var year = document.getElementById("year");
-    var month = document.getElementById("month");
-    var month = document.getElementById("day");
-    var month = document.getElementById("gender");
+    var year = document.getElementById("year").value;
+    var month = document.getElementById("month").value;
+    var month = document.getElementById("day").value;
+    var month = document.getElementById("gender").value;
 }
