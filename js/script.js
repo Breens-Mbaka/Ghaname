aba8aca8c1cc6c4a71fd4f32a17fbd97420e4c5f
Breens-Thomas <mbakabreens@gmail.com>
2020-12-13 17:24:16 +0300

    Had an error naming the variables the same thus clicking on button did not call on the akanNaming()function

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ function akanNaming() {
     //access input data and assign them to a variable
     var year = document.getElementById("year").value;
     var month = document.getElementById("month").value;
-    var month = document.getElementById("day").value;
-    var month = document.getElementById("gender").value;
+    var day = document.getElementById("day").value;
+    var gender = document.getElementById("gender").value;
 
     //calculation to get the century which the user was born in
     var century = Math.trunc(Math.round(year * 0.01));
@@ -15,7 +15,7 @@ function akanNaming() {
     var dayOfTheWeek = Math.round((((century/4)-2*century-1)+((5*year/4))+((26*(month+1)/10))+day)%7);
 
     //this condition must be true so that the nested if/else if statements can be executed
-    if(gender == "male") {
+    if(gender === "male") {
         if(dayOfTheWeek === 0) {
             alert("Hey your Akan name is: " + akanMaleNames[0]);
         }
