592caea1b6b66776d0c7e99221c62a9dd81f84d1
Breens-Thomas <mbakabreens@gmail.com>
2020-12-13 11:21:41 +0300

    Add gender parameter and also assigned the data gotten from the user to the function arguments

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function akanNaming(century,year,month,day) {
+function akanNaming(century,year,month,day,gender) {
     var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
@@ -9,3 +9,6 @@ var century = document.getElementById("century").value;
 var year = document.getElementById("year").value;
 var month = document.getElementById("month").value;
 var day = document.getElementById("day").value;
+var gender = document.getElementById("gender").value;
+
+akanNaming(century,year,month,day,gender);
