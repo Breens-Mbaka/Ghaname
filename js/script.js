0329be5f7258db7f473a396386ade08bdc392fc4
Breens-Thomas <mbakabreens@gmail.com>
2020-12-13 16:47:27 +0300

    Used the year user was born to calculate which century the user was born in

--- js/script.js
+++ js/script.js
@@ -3,8 +3,12 @@ function akanNaming() {
     var akanMaleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     var akanFemaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
+    //access input data and assign them to a variable
     var year = document.getElementById("year").value;
     var month = document.getElementById("month").value;
     var month = document.getElementById("day").value;
     var month = document.getElementById("gender").value;
+
+    //formula to get the century which someone was born in
+    var century = Math.trunc(Math.round(year * 0.01));
 }
