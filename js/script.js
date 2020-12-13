35abcc2e0b4da4ad956cf42d65f68fa3fe856a6d
Breens-Thomas <mbakabreens@gmail.com>
2020-12-13 16:34:43 +0300

    Add a list of male and female Akan names

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 //function for giving Akan names according to which day of the week a user was born
 function akanNaming() {
-
+    var akanMaleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var akanFemaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 }
