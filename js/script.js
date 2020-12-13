c1a479faf8da916ecf52af1e69fe14ce09c07eac
Breens-Thomas <mbakabreens@gmail.com>
2020-12-13 11:14:16 +0300

    Get century,year,month and data from the form by referring to the inputs with their unique id's

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 function akanNaming() {
-    
+    var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+
 }
+
+var century = document.getElementById("century");
+var year = document.getElementById("year");
+var month = document.getElementById("month");
+var day = document.getElementById("day");
