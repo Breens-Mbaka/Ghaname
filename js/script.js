271634b48e1f52bc571da05b4c6658f0cb07c701
Breens-Thomas <mbakabreens@gmail.com>
2020-12-13 16:51:09 +0300

    Calculation to know which day of the week the user was born

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@ function akanNaming() {
     var month = document.getElementById("day").value;
     var month = document.getElementById("gender").value;
 
-    //formula to get the century which someone was born in
+    //calculation to get the century which the user was born in
     var century = Math.trunc(Math.round(year * 0.01));
+    //calculation to know which day of the week the user was born
+    var dayOfTheWeek = Math.round((((century/4)-2*century-1)+((5*year/4))+((26*(month+1)/10))+day)%7);
 }
